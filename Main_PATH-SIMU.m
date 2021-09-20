e33a44f6de415b9d9badc8c53e747cddef6c1c4f
fmoissenet <florent.moissenet@protonmail.com>
2021-09-20 14:02:43 +0200

    add Main_PATH-SIMU

---
+++ Main_PATH-SIMU.m
@@ -0,0 +1,40 @@
+% Author       : F. Moissenet
+%                Kinesiology Laboratory (K-LAB)
+%                University of Geneva
+%                https://www.unige.ch/medecine/kinesiology
+% License      : Creative Commons Attribution-NonCommercial 4.0 International License 
+%                https://creativecommons.org/licenses/by-nc/4.0/legalcode
+% Source code  : https://github.com/fmoissenet/NSLBP-BIOToolbox
+% Reference    : To be defined
+% Date         : September 2021
+% -------------------------------------------------------------------------
+% Description  : Main routine used to launch PATH-SIMU routines
+% -------------------------------------------------------------------------
+% Dependencies : To be defined
+% -------------------------------------------------------------------------
+% This work is licensed under the Creative Commons Attribution - 
+% NonCommercial 4.0 International License. To view a copy of this license, 
+% visit http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to 
+% Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+% -------------------------------------------------------------------------
+
+% -------------------------------------------------------------------------
+% INIT THE WORKSPACE
+% -------------------------------------------------------------------------
+clearvars;
+close all;
+clc;
+
+% -------------------------------------------------------------------------
+% SET FOLDERS
+% -------------------------------------------------------------------------
+disp('Set folders');
+Folder.toolbox      = 'C:\Users\moissene\OneDrive - unige.ch\2021 - PATH-SIMU\PATH-SIMU_Toolbox\';
+Folder.data         = '';
+Folder.dependencies = [Folder.toolbox,'dependencies\'];
+addpath(Folder.toolbox);
+addpath(genpath(Folder.dependencies));
+cd(Folder.toolbox);
+
+% Test btk
+btkGetVersion
